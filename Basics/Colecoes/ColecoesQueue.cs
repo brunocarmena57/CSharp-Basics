50e2d9b1defee72c29d4b977d5bcfe011bb763d2
Bruno Ricardo Carmena <106131265+brunocarmena57@users.noreply.github.com>
2023-01-29 23:13:16 -0300

    Update ColecoesQueue.cs

--- Basics/Colecoes/ColecoesQueue.cs
+++ Basics/Colecoes/ColecoesQueue.cs
@@ -2,7 +2,8 @@
 using System.Collections;
 using System.Collections.Generic;
 
-namespace CursoCSharp.Colecoes {
+namespace CursoCSharp.Colecoes 
+{
     class ColecoesQueue
     {
         public static void Executar() {
